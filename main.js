ac12be6f501396b62dca75ff3447f40fb2a84ae4
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-06-01 22:41:19 +0100

    Cam Switching

--- main.js
+++ main.js
@@ -1,32 +1,68 @@
 // Imports ----------------------------
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { TTFLoader } from "three/examples/jsm/loaders/TTFLoader";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 // ------------------------------------
 
-
-
 // Init Setup -------------------------
-
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10000
+);
+var camPos = 1;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 const render = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector("#bg"),
 });
 
 // ------------------------------------
 
-
 // Camera Setup -----------------------
+function camSetup() {
+  camera.position.set(0, 0, 5.3);
+}
 
-camera.position.set(0, 0 , 7);
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyC":
+      if (camPos == 1) {
+        // Camera 2 - Longe Cima
+        scene.remove(camera);
+        camera = new THREE.OrthographicCamera(
+          window.innerWidth / -2,
+          window.innerWidth / 2,
+          window.innerHeight / 2,
+          window.innerHeight / -2,
+          1,
+          1000
+        );
+        camera.position.set(0, 7, 7);
+        scene.add(camera);
+        camPos = 2;
+      } else if (camPos == 2) {
+        // Camera 1 - Perto Tras
+        scene.remove(camera);
+        camera = new THREE.PerspectiveCamera(
+          75,
+          window.innerWidth / window.innerHeight,
+          0.1,
+          10000
+        );
+        camera.position.set(0, 0, 5.3);
+        scene.add(camera);
+        camPos = 1;
+      }
+      break;
+  }
+});
 
 // ------------------------------------
-
 
 // Render Setup -----------------------
 
@@ -44,17 +80,16 @@ capi.position.set(0, -10, -50);
 scene.add(capi);
 // ------------------------------------
 
-
 // Font Test---------------------------
 
 const fontLoader = new FontLoader();
 const ttfLoader = new TTFLoader();
 
-ttfLoader.load('assets/fonts/Bungee-Regular.ttf', (json) => {
+ttfLoader.load("assets/fonts/Bungee-Regular.ttf", (json) => {
   // First parse the font.
   const BungeeFont = fontLoader.parse(json);
   // Use parsed font as normal.
-  const textGeometry = new TextGeometry('CapiRush', {
+  const textGeometry = new TextGeometry("CapiRush", {
     height: 2,
     size: 10,
     font: BungeeFont,
@@ -68,17 +103,20 @@ ttfLoader.load('assets/fonts/Bungee-Regular.ttf', (json) => {
 
 // ------------------------------------
 
-
-
 function animate() {
   requestAnimationFrame(animate); // First
 
   // ------------------------------------
 
-
-
   // ------------------------------------
 
   render.render(scene, camera); // Last
 }
-animate();
+
+function start() {
+  camSetup();
+
+  animate();
+}
+
+start();
